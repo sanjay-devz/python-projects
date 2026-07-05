49150f7ba1349802e1643e49d4f848106236b859
sanjay-devz <sanjay.gridz@gmail.com>
2026-07-05 14:26:31 +0530

    the code was added

---
+++ 01-python-fundamentals/02-deepseek-debugs.py
@@ -0,0 +1,24 @@
+#the error code 
+
+# fahrenheit = input("Enter temp in F:")
+# celsius = (fahrenheit - 32) * 5/9
+# print("Temperature in C:" celsius)
+
+#debugged code
+
+fahrenheit = int(input("Enter temp in F:"))
+celsius = (fahrenheit -32) * 5/9
+print("Temperatur in C:", celsius)
+
+
+#output 
+# #the error code 
+
+# fahrenheit = input("Enter temp in F:")
+# celsius = (fahrenheit - 32) * 5/9
+# print("Temperature in C:" celsius)
+
+#debugged code
+# Enter temp in F:23
+# #Temperatur in C: -5.0
+
