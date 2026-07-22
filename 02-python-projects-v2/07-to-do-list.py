3728505e151a29b910f7aea1a354da5f50c4716f
sanjay-devz <sanjay.gridz@gmail.com>
2026-07-22 09:41:57 +0530

    files location was changed

--- 02-python-projects-v2/07-to-do-list.py
+++ 02-python-projects-v2/07-to-do-list.py
@@ -1,29 +1,4 @@
-task_list = []
-
-while True:
-    print("=" * 30)
-    print("=" * 20)
-    print("\t--TO DO LIST--")
-    print("=" * 30)
-    print("=" * 20)
-
-    print("\n1.AddTask\n2.View Task\n3.Mark Task as completed\n4.Delete Task\n5.Exit\n")
-
-    choice = int(input("Enter Your Choice: "))
-
-    if choice == 5:
-        print("Good Bye!!\nSee You Later!!")
-        break
-        
-    elif choice == 1:
-        new_task = input("Enter Task: ")
-    
-        task_list.append(new_task)
-
-        ask_user = input("Enter [Y/N] ->").upper()
-
-    elif choice == 2:
-        for x in task_list:
-            
-            print("[]",x)
-           
+a = "adam"
+print(a.upper())
+print(a.capitalize())
+print(a. )
